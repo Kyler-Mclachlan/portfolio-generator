9f8a45f788697f05cbcb573a2b6e94d582415fa4
Kyler Mclachlan <51749588+Kyler-Mclachlan@users.noreply.github.com>
2022-01-16 21:04:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i++){
-  console.log(ProfileData(profileDataArgs[i]);
+  for (let i = 0; i < profileDataArr.length; i++) {
+  console.log(profileDataArr[i]);
   }
 };
